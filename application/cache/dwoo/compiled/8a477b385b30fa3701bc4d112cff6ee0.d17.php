04431af3f149bc3c70bcaccfcdcff24fa3240229
Đỗ Tuấn <tuan.dongoc@gmail.com>
2013-09-07 21:56:23 +0700

    delete

--- application/cache/dwoo/compiled/8a477b385b30fa3701bc4d112cff6ee0.d17.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* template head */
-/* end template head */ ob_start(); /* template body */ ?><ul id="root">
-    <li><a href="javascript:">1.Game Parameters</a></li>
-    <li><a href="javascript:">2.Stations</a> </li>
-    <li><a href="javascript:">3.Teams</a> </li>
-</ul><?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
--- application/language/vietnam/sn_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Thông tin hiển thị trang chủ
-$lang['sn.sitename'] = "Social Network";
-$lang['sn.title'] = "Mạng xã hội kết bạn trực tuyến dành cho giới trẻ";
-$lang['sn.slogan'] = "Kết bạn trực tuyến";
-
